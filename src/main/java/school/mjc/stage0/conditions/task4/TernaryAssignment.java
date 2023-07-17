a404211c89e551dbad01f2532ad71df6bcd78624
Sardor0305 <87496899+Sardor0305@users.noreply.github.com>
2023-07-17 16:10:26 +0500

    Update TernaryAssignment.java

--- src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
+++ src/main/java/school/mjc/stage0/conditions/task4/TernaryAssignment.java
@@ -4,6 +4,6 @@ public class TernaryAssignment {
     public void assignAndPrintBasedOnWhichBigger(int first, int second) {
          int result = (first > second) ? 10 : -10;
         System.out.println(result);
-    }
+    
     }
 }
